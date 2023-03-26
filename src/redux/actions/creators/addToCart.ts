e7dc339418c65dc46aef0629f14c5b7dabec08ce
piauming <piauming>
2023-03-26 18:02:21 +0800

    corrected action-creator's input to be independent of the payload format

--- src/redux/actions/creators/addToCart.ts
+++ src/redux/actions/creators/addToCart.ts
@@ -3,9 +3,13 @@ import { Action } from "../index";
 import { ActionType } from "../type";
 import { CartItem } from "../payload";
 
-const addToCart = (cartItem: CartItem) => {
+const addToCart = (id: number, count: number, title: string) => {
     return (dispatch: Dispatch<Action>) => {
-        // action is the value, and the value must match the "interface AddToCartAction"
+        // action is the value
+        // this action creator can specify what are the inputs but
+        // and the format of payload must match 
+        // the "interface AddToCartAction" in "redux/actions/index"
+        const cartItem: CartItem = {id: id, count: count, title: title}
         const action = {
             type: ActionType.ADD_TO_CART,
             payload: cartItem
